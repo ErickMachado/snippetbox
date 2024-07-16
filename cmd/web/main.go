fb627f5e8d4c978c8d876baf937d90b970e0f3c0
Erick Machado <erick.gsmachado@gmail.com>
2024-07-16 18:15:46 -0300

    ✅ chapter 4.5 completed

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,11 +7,13 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/ErickMachado/snippetbox/internal/models"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
-	logger *slog.Logger
+	logger   *slog.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
@@ -29,6 +31,9 @@ func main() {
 
 	app := &application{
 		logger: logger,
+		snippets: &models.SnippetModel{
+			DB: db,
+		},
 	}
 
 	logger.Info("starting server", "addr", *addr)
