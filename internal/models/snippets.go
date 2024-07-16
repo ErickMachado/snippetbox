351817c166e597ad8101486d46a3076d69d5ee30
Erick Machado <erick.gsmachado@gmail.com>
2024-07-16 19:13:38 -0300

    ✅ chapter 4.7 completed

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -46,7 +47,36 @@ func (m *SnippetModel) Insert(title, content string, expires int) (int, error) {
 }
 
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	return Snippet{}, nil
+	stmt := `
+		SELECT
+			id,
+			title,
+			content,
+			created,
+			expires
+		FROM snippets
+		WHERE id = ?
+			AND expires > UTC_TIMESTAMP()
+	`
+	var s Snippet
+
+	err := m.DB.QueryRow(stmt, id).Scan(
+		&s.ID,
+		&s.Title,
+		&s.Content,
+		&s.Created,
+		&s.Expires,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		} else {
+			return Snippet{}, err
+		}
+	}
+
+	return s, nil
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
