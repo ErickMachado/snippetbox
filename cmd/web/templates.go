b38c69abd80ab7fdc5c732c012c12c7021254d0c
Erick Machado <erick.gsmachado@gmail.com>
2024-07-17 19:52:16 -0300

    ✅ chapter 5.3 completed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,43 @@
 package main
 
-import "github.com/ErickMachado/snippetbox/internal/models"
+import (
+	"html/template"
+	"path/filepath"
+
+	"github.com/ErickMachado/snippetbox/internal/models"
+)
 
 type templateData struct {
 	Snippet  models.Snippet
 	Snippets []models.Snippet
+}
+
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
+	pages, err := filepath.Glob("./ui/html/pages/*.tmpl")
+	if err != nil {
+		return nil, err
+	}
+
+	for _, page := range pages {
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		name := filepath.Base(page)
+		ts, err = ts.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+
+		cache[name] = ts
+	}
+
+	return cache, nil
 }
