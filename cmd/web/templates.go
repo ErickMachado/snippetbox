c28a89a1f7b0e4045701321816fe19490a381965
Erick Machado <erick.gsmachado@gmail.com>
2024-07-17 19:19:12 -0300

    ✅ chapter 5.2 completed

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,5 +3,6 @@ package main
 import "github.com/ErickMachado/snippetbox/internal/models"
 
 type templateData struct {
-	Snippet models.Snippet
+	Snippet  models.Snippet
+	Snippets []models.Snippet
 }
