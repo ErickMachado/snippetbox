945bb36823625f741e178120e5f48f555483a63b
Erick Machado <erick.gsmachado@gmail.com>
2024-07-24 08:44:14 -0300

    ✅ chapter 10.2 completed

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,6 +20,7 @@ import (
 type application struct {
 	logger         *slog.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -50,10 +51,9 @@ func main() {
 	sessionManager.Lifetime = 12 * time.Hour
 
 	app := &application{
-		logger: logger,
-		snippets: &models.SnippetModel{
-			DB: db,
-		},
+		logger:         logger,
+		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
