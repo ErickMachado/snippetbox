34a04fa01b93a32665700685c6679796cc032252
Erick Machado <erick.gsmachado@gmail.com>
2024-07-26 17:24:59 -0300

    ✅ chapter 12.2 completed

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,17 @@ package main
 import (
 	"net/http"
 
+	"github.com/ErickMachado/snippetbox/ui"
 	"github.com/justinas/alice"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(app.sessionManager.LoadAndSave, noSurf, app.authenticate)
 
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
 	mux.Handle("GET /snippet/view/{id}", dynamic.ThenFunc(app.snippetView))
 	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
